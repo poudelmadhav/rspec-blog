ad1f1c7c5aede59fcd291f447f7dc28f1bf2f414
Madhav Paudel <poudelmadhav143@gmail.com>
2018-03-05 19:02:18 +0545

    CRUD empty methods added in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
 	def index
 		
 	end
+
+	def show
+		
+	end
+
+	def create
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
