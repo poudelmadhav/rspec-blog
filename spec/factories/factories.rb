43a744a5bd98f35916693429b9ebdc2cd47887ff
Madhav Paudel <poudelmadhav143@gmail.com>
2018-03-07 17:27:34 +0545

    testing invalid create actions

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     title "Birthday Wish"
     author  "Madhav Paudel"
     content "Happy Birthday Ganga. All the best wishes. Be happy always... Have a nice journey of life!"
+    factory :invalid_post do
+    	content "Short Content"
+    end
   end
 end
