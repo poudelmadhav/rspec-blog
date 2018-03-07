095325526aaa218f7e4199479e89cb778f622496
Madhav Paudel <poudelmadhav143@gmail.com>
2018-03-07 16:53:14 +0545

    validation added in post.rb

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :post do
     title "Birthday Wish"
     author  "Madhav Paudel"
-    content "Happy Birthday Ganga. All the best wishes."
+    content "Happy Birthday Ganga. All the best wishes. Be happy always... Have a nice journey of life!"
   end
 end
